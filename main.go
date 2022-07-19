1e108d27fe8636941b0acf6fbc564abe25a58ec9
dchf <dchf.jp@gmail.com>
2022-07-19 10:58:36 +0900

    Add max

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type segmentTreeData struct {
 	prefix int
@@ -8,10 +10,16 @@ type segmentTreeData struct {
 	best   int
 	total  int
 }
+type segmentTree struct {
+	n    int
+	data []segmentTreeData
+}
 
 func main() {
 	s := createTree(1)
 	fmt.Printf("%v", s)
+	fmt.Printf("%v", merge(s, s))
+
 }
 func createTree(v int) segmentTreeData {
 	return segmentTreeData{
@@ -24,9 +32,9 @@ func createTree(v int) segmentTreeData {
 
 func merge(left, right segmentTreeData) segmentTreeData {
 	total := left.total + right.total
-	prefix := max(left.prefix, left.total+right.prefix)
-	suffix := max(right.suffix, right.total+left.suffix)
-	best := max(
+	prefix := Max(left.prefix, left.total+right.prefix)
+	suffix := Max(right.suffix, right.total+left.suffix)
+	best := Max(
 		left.best,
 		right.best,
 		prefix,
@@ -39,4 +47,18 @@ func merge(left, right segmentTreeData) segmentTreeData {
 		best,
 		total,
 	}
+}
+
+func max(l, r int) int {
+	if l > r {
+		return l
+	}
+	return r
+}
+func Max(n ...int) int {
+	var m int
+	for _, v := range n {
+		m = max(m, v)
+	}
+	return m
 }
